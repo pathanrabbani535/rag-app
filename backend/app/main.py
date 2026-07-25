76aa0807f6a44f5599279d9be949833fbf397055
pathanrabbani535 <pathanrabbani535@gmail.com>
2026-07-25 17:19:29 +0530

    Update Uvicorn app import path in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,4 +32,4 @@ os.makedirs(settings.UPLOAD_DIR, exist_ok=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render gives PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
